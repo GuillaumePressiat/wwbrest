9839d20c97a2c82783234515cb5e84be20b1dad3
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2020-05-30 13:02:55 +0200

    Reduce full images scale

--- compress_with_magick.R
+++ compress_with_magick.R
@@ -4,13 +4,13 @@ library(magick)
 
 setwd('~/Pictures/wwbrest/wwbrest/images/')
 dir.create('c')
-files <- list.files(pattern = "jpg|JPG")
+files <- list.files('f', pattern = "jpg|JPG")
 i <- files[1]
 compression <- function(i){
   # image_read(i, density = NULL, depth = NULL, strip = FALSE) %>% 
   #   image_write(path = paste0('c/', i), format = "jpeg", quality = 15)
   
-  im <- image_read(i, density = NULL, depth = NULL, strip = FALSE)
+  im <- image_read(paste0('f/', i), density = NULL, depth = NULL, strip = FALSE)
   w <- im %>% image_info() %>% .$width / 13
   
   im %>% 
@@ -19,3 +19,17 @@ compression <- function(i){
 }
 
 files %>% purrr::map(compression)
+
+compression_light <- function(i){
+  # image_read(i, density = NULL, depth = NULL, strip = FALSE) %>% 
+  #   image_write(path = paste0('c/', i), format = "jpeg", quality = 15)
+  
+  im <- image_read(paste0('f/', i), density = NULL, depth = NULL, strip = FALSE)
+  w <- im %>% image_info() %>% .$width / 4
+  
+  im %>% 
+    image_scale(geometry = w) %>% 
+    image_write(path = i, format = "jpeg", quality = 100)
+}
+
+files %>% purrr::map(compression_light)
